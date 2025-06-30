798a19777d86b809a498b33fffe1aafe88b3ac0a
Lionel Ngawa <160720448+lnk99@users.noreply.github.com>
2025-06-30 12:48:11 -0500

    Update LSTM_code.py

--- LSTM_code.py
+++ LSTM_code.py
@@ -1,1 +1,17 @@
 !pip install --upgrade scikit-learn==1.0 --quiet
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns # viz
+import matplotlib.pyplot as plt # viz
+from scipy import stats
+import json
+from typing import List, Tuple
+
+from sklearn.ensemble import IsolationForest
+from sklearn.metrics import f1_score, balanced_accuracy_score, roc_auc_score, precision_recall_fscore_support
+from sklearn import metrics, linear_model
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
